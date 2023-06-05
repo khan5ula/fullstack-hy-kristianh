6302dc4d7810876194617180acdaedf2869ed6f6
Kristian Hannula <kristian.hannula@proton.me>
2023-06-05 22:39:51 +0300

    init

---
+++ code_along_project/backend/mongo.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('given password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://kristianfullstack:${password}@kristian-fullstack.8ihwvmx.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
+
+/* ------- SECTION FOR CREATING NEW NOTES ------- */
+/*
+const note = new Note({
+    content: 'HTML is Easy',
+    important: true,
+})
+
+const test = new Note({
+    content: 'Giggels goggels',
+    important: true,
+})
+
+const secondTest = new Note({
+    content: 'Not so giggels gäggels',
+    important: false,
+})
+*/
+
+/* MongoDB operations are asynchronous, so all save operations should be executed before the connection is closed */
+
+/*
+Promise.all([
+    note.save().then(() => console.log('note ' + note.content + ' saved!')),
+    test.save().then(() => console.log('note ' + test.content + ' saved!')),
+    secondTest.save().then(() => console.log('note ' + secondTest.content + ' saved!')),
+]).then(results => {
+    mongoose.connection.close()
+}).catch(error => {
+    console.log('error:', error)
+    mongoose.connection.close()
+})
+*/
+/* ------- SECTION FOR CREATING NEW NOTES ------- */
