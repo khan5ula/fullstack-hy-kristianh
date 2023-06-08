2b76637ced5370f866806d7228af7239b5def860
Kristian Hannula <kristian.hannula@proton.me>
2023-06-08 12:49:02 +0300

    Did some supertests

--- code_along_project/backend/mongo.js
+++ code_along_project/backend/mongo.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
@@ -8,7 +9,8 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-    `mongodb+srv://kristianfullstack:${password}@kristian-fullstack.8ihwvmx.mongodb.net/noteApp?retryWrites=true&w=majority`
+  //`mongodb+srv://kristianfullstack:${password}@kristian-fullstack.8ihwvmx.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://kristianhannula:${password}@notestesting.zbfw603.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -20,43 +22,37 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema)
 
-Note.find({}).then(result => {
+/*Note.find({}).then(result => {
   result.forEach(note => {
     console.log(note)
   })
   mongoose.connection.close()
-})
+})*/
 
 /* ------- SECTION FOR CREATING NEW NOTES ------- */
-/*
+
 const note = new Note({
-    content: 'HTML is Easy',
-    important: true,
+  content: 'HTML is Easy',
+  important: true,
 })
 
 const test = new Note({
-    content: 'Giggels goggels',
-    important: true,
+  content: 'Giggels goggels',
+  important: true,
 })
 
-const secondTest = new Note({
-    content: 'Not so giggels gäggels',
-    important: false,
-})
-*/
 
 /* MongoDB operations are asynchronous, so all save operations should be executed before the connection is closed */
 
-/*
+
 Promise.all([
-    note.save().then(() => console.log('note ' + note.content + ' saved!')),
-    test.save().then(() => console.log('note ' + test.content + ' saved!')),
-    secondTest.save().then(() => console.log('note ' + secondTest.content + ' saved!')),
+  note.save().then(() => console.log('note ' + note.content + ' saved!')),
+  test.save().then(() => console.log('note ' + test.content + ' saved!')),
 ]).then(results => {
-    mongoose.connection.close()
+  mongoose.connection.close()
 }).catch(error => {
-    console.log('error:', error)
-    mongoose.connection.close()
+  console.log('error:', error)
+  mongoose.connection.close()
 })
-*/
+
 /* ------- SECTION FOR CREATING NEW NOTES ------- */
