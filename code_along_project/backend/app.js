61b789d814c6f2e0aac6ea195cd2b088e20cd261
Kristian Hannula <kristian.hannula@proton.me>
2023-06-10 19:18:21 +0300

    Continued codealong until 'muistiinpanon luominen'

--- code_along_project/backend/app.js
+++ code_along_project/backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -24,9 +25,8 @@ app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
-
 app.use('/api/notes', notesRouter)
-
+app.use('/api/users', usersRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
--- code_along_project/backend/controllers/notes.js
+++ code_along_project/backend/controllers/notes.js
@@ -6,16 +6,6 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response) => {
-  const note = await Note.findById(request.params.id)
-  if (note) {
-    response.json(note)
-  } else {
-    // eslint-disable-next-line no-undef
-    response.status(201).json(savedNote)
-  }
-})
-
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -25,7 +15,16 @@ notesRouter.post('/', async (request, response) => {
   })
 
   const savedNote = await note.save()
-  response.json(savedNote)
+  response.status(201).json(savedNote)
+})
+
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 notesRouter.delete('/:id', async (request, response) => {
